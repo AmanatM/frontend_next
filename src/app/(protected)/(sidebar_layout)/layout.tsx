3ce3a12c3c49437c9721b52c94ad7aba3b6fbd8c
Ama <24210834+AmanatM@users.noreply.github.com>
2024-04-04 12:32:50 -0700

    code question is in separate layout

---
+++ src/app/(protected)/(sidebar_layout)/layout.tsx
@@ -0,0 +1,22 @@
+import { MenuTopBar } from '@/components/Header'
+import Sidebar from '@/components/Sidebar'
+
+export default async function SidebarLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <div className="relative h-full bg-background">
+      <MenuTopBar />
+
+      <div className="flex flex-row pt-14">
+        <Sidebar />
+
+        <main id="content" className="grow">
+          {children}
+        </main>
+      </div>
+    </div>
+  )
+}
--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,31 +1,19 @@
 import { MenuTopBar } from '@/components/Header'
-import Sidebar from '@/components/Sidebar'
+import { cn } from '@/lib/utils'
 import { createClient } from '@/supabase-utils/supabase-server'
 import { redirect } from 'next/navigation'
 
-export default async function MainLayout({
+export default async function ProtectedLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   const supabase = createClient()
-
   const { data, error } = await supabase.auth.getUser()
+
   if (error || !data?.user) {
     redirect('/login')
   }
 
-  return (
-    <div className="relative h-full bg-background">
-      <MenuTopBar />
-
-      <div className="flex flex-row pt-14">
-        <Sidebar />
-
-        <main id="content" className="grow">
-          {children}
-        </main>
-      </div>
-    </div>
-  )
+  return <>{children}</>
 }
