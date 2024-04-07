695167c46953f02bfe7db621315ee87a74f204e8
Ama <24210834+AmanatM@users.noreply.github.com>
2024-04-07 11:09:24 -0700

    fix

--- src/app/(auth)/actions/index.tsx
+++ src/app/(auth)/actions/index.tsx
@@ -1,24 +1,23 @@
 'use server'
 
-import { createClient } from '@/supabase-utils/supabase-server'
-import { redirect } from 'next/navigation'
+import { createClientServer } from '@/supabase-utils/supabase-server'
 
 export async function loginWithEmailAndPassword(data: { email: string; password: string }) {
-  const supabase = await createClient()
+  const supabase = await createClientServer()
 
   const result = await supabase.auth.signInWithPassword(data)
   return JSON.stringify(result)
 }
 
 export async function signUpWithEmailAndPassword(data: { email: string; password: string }) {
-  const supabase = await createClient()
+  const supabase = await createClientServer()
 
   const result = await supabase.auth.signUp(data)
   return JSON.stringify(result)
 }
 
 export async function logout() {
-  const supabase = await createClient()
+  const supabase = await createClientServer()
   await supabase.auth.signOut()
   // redirect('/auth')
 }
--- src/app/tutorials/[id]/page.tsx
+++ src/app/tutorials/[id]/page.tsx
@@ -1,3 +1,14 @@
-export default function Page({ params }: { params: { slug: string } }) {
-  return <div>My Post: {params.slug}</div>
+import { createClientServer } from '@/supabase-utils/supabase-server'
+
+export default async function TutorialRoute({ params }: { params: { id: string } }) {
+  const supabase = await createClientServer()
+
+  const { data: tutorial } = await supabase.from('tutorials').select().single()
+
+  return (
+    <div>
+      <h1>Tutorial {params.id}</h1>
+      <h1>{tutorial?.title}</h1>
+    </div>
+  )
 }
