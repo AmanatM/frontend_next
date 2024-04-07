eada7d3ef69a1d918c45c02237a9b244515aeda4
Ama <24210834+AmanatM@users.noreply.github.com>
2024-04-07 13:52:00 -0700

    coding questions initial layout

--- src/app/(dashboard)/coding/page.tsx
+++ src/app/(dashboard)/coding/page.tsx
@@ -1,8 +1,11 @@
 import { Button } from '@/components/custom/button'
 import { DashboardContainer } from '@/components/dashboard-container'
 import { TypographyH3, TypographyH4, TypographyMuted } from '@/components/typography'
+import { Input } from '@/components/ui/input'
 import { Separator } from '@/components/ui/separator'
-import { CodeXml, Dock } from 'lucide-react'
+import { Skeleton } from '@/components/ui/skeleton'
+import { Toggle } from '@/components/ui/toggle'
+import { CodeXml, Dock, LayoutGrid, PanelTop, Search, SquareChevronRight } from 'lucide-react'
 import Link from 'next/link'
 
 export default function Coding() {
@@ -12,26 +15,36 @@ export default function Coding() {
         <TypographyH3>Coding Questions</TypographyH3>
         <TypographyMuted>Two layout options</TypographyMuted>
       </div>
-      <div className="space-y-3">
-        <TypographyH4>1. User interface layout: </TypographyH4>
-        <Button className="gap-x-2 mr-2" leftIcon={<Dock size={17} />} asChild>
-          <Link href={'/coding/1'}>Styling Text with css</Link>
-        </Button>
-
-        <Button className="gap-x-2" leftIcon={<Dock size={17} />} asChild>
-          <Link href={'/coding/2'}>Creating a navbar</Link>
-        </Button>
-
-        <Button className="gap-x-2" leftIcon={<Dock size={17} />} asChild>
-          <Link href={'/coding/9283'}>Sandpack</Link>
-        </Button>
-      </div>
-      <Separator />
-      <div className="space-y-3">
-        <TypographyH4>2. Javascript layout: </TypographyH4>
-        <Button className="gap-x-2" leftIcon={<CodeXml size={17} />} asChild>
-          <Link href={'/'}>Javascript layout</Link>
-        </Button>
+      <div className="">
+        <div className="space-y-3">
+          <div className="flex space-x-3">
+            <Toggle variant={'outline'} className="space-x-2">
+              <SquareChevronRight size={15} className="text-muted-foreground" />
+              <TypographyMuted className="text-sm">JavaScript</TypographyMuted>
+            </Toggle>
+            <Toggle variant={'outline'} className="space-x-2">
+              <PanelTop size={15} className="text-muted-foreground" />
+              <TypographyMuted className="text-sm">User Interface</TypographyMuted>
+            </Toggle>
+            <Toggle variant={'outline'} className="space-x-2">
+              <LayoutGrid size={15} className="text-muted-foreground" />
+              <TypographyMuted className="text-sm">Other</TypographyMuted>
+            </Toggle>
+          </div>
+          <div className="bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+            <form>
+              <div className="relative">
+                <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+                <Input placeholder="Search" className="pl-8" />
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="mt-3 flex flex-col space-y-3">
+          {Array.from({ length: 20 }).map((_, index) => (
+            <Skeleton key={index} className="h-14" />
+          ))}
+        </div>
       </div>
     </DashboardContainer>
   )
