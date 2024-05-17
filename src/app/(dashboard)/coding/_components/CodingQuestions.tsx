e8b54c9ff7f91a952e14af8e8c67081167053f01
Ama <24210834+AmanatM@users.noreply.github.com>
2024-05-17 10:29:27 -0700

    added client side filtering

--- src/app/(dashboard)/coding/_components/CodingQuestions.tsx
+++ src/app/(dashboard)/coding/_components/CodingQuestions.tsx
@@ -1,56 +1,73 @@
 'use client'
-import { TypographyP } from '@/components/typography'
+import { TypographyMuted, TypographyP } from '@/components/typography'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { cn } from '@/lib/utils'
 import { useQuery } from '@tanstack/react-query'
-import { CircleIcon, StarIcon } from 'lucide-react'
+import { AppWindow, CircleIcon, Code, Gauge, StarIcon } from 'lucide-react'
 import Link from 'next/link'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { getCodingQuestions } from '../_queries/getCodingQuestions'
 import useSupabaseBrowser from '@/supabase-utils/supabase-client'
 import { Badge } from '@/components/ui/badge'
+import { useSearchParams } from 'next/navigation'
 
 const CodingQuestions = () => {
   const supabase = useSupabaseBrowser()
+  const params = useSearchParams()
+  const type = params.get('type')
 
   const { data: codingQuestions, isSuccess } = useQuery({
     queryKey: ['codingQuestions'],
     queryFn: () => getCodingQuestions(supabase),
   })
 
-  if (!isSuccess) return null
+  const filteredCodingQuestions = useMemo(() => {
+    if (type && codingQuestions) {
+      return codingQuestions.filter(question => question.question_type === type)
+    }
+    return codingQuestions
+  }, [codingQuestions, type])
 
-  return (
-    <div>
-      {codingQuestions.map(question => (
-        <Link key={question.id} href={`/coding/${question.id}`} className="flex">
-          <Card className="size-full px-1 py-1">
-            <CardHeader className="flex flex-row space-x-2 items-middle justify-between">
-              <TypographyP>{question.title}</TypographyP>
-              {question.user_completed_code_question.length > 0 && <Badge variant="secondary">Completed</Badge>}
-            </CardHeader>
-            <CardContent>
-              <div className="flex space-x-4 text-sm text-muted-foreground">
-                <div className="flex items-center">
-                  <CircleIcon
-                    className={cn(
-                      'mr-1 h-3 w-3 fill-sky-400 text-sky-400',
-                      question.with_browser ? 'text-sky-400' : 'text-amber-700-500',
+  if (filteredCodingQuestions && codingQuestions)
+    return (
+      <div className="flex flex-col space-y-1">
+        {filteredCodingQuestions.map(question => (
+          <Link key={question.id} href={`/coding/${question.id}`} className="flex">
+            <div className="size-full space-y-4 px-6 py-4 bg-primary-foreground rounded-sm">
+              <div className="flex flex-row space-x-4">
+                <TypographyP>{question.title}</TypographyP>
+                {question.user_completed_code_question.length > 0 && (
+                  <Badge variant="outline" className="">
+                    Completed
+                  </Badge>
+                )}
+              </div>
+              <div>
+                <div className="flex space-x-4 text-sm text-muted-foreground">
+                  <div className="flex items-center min-w-[140px]">
+                    {question.question_type === 'user_interface' ? (
+                      <>
+                        <AppWindow size={15} className={cn('mr-2')} />
+                        <TypographyMuted>User Interface</TypographyMuted>
+                      </>
+                    ) : (
+                      <>
+                        <Code size={15} className={cn('mr-2')} />
+                        <TypographyMuted>Coding</TypographyMuted>
+                      </>
                     )}
-                  />
-                  {question.with_browser ? 'User Interface' : 'Java Script'}
-                </div>
-                <div className="flex items-center">
-                  <StarIcon className="mr-1 h-3 w-3" />
-                  20k
+                  </div>
+                  <div className="flex items-center">
+                    <Gauge size={15} className={cn('mr-2')} />
+                    <TypographyMuted>{question.difficulty}</TypographyMuted>
+                  </div>
                 </div>
               </div>
-            </CardContent>
-          </Card>
-        </Link>
-      ))}
-    </div>
-  )
+            </div>
+          </Link>
+        ))}
+      </div>
+    )
 }
 
 export default CodingQuestions
--- src/app/(dashboard)/coding/_components/SearchFilter.tsx
+++ src/app/(dashboard)/coding/_components/SearchFilter.tsx
@@ -1,32 +1,56 @@
+'use client'
 import { TypographyMuted } from '@/components/typography'
 import { Input } from '@/components/ui/input'
-import { Toggle } from '@/components/ui/toggle'
-import { SquareChevronRight, PanelTop, LayoutGrid, Search } from 'lucide-react'
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
+import { SquareChevronRight, PanelTop, Search, List } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useMemo, useState } from 'react'
 
 const SearchFilter = () => {
+  const router = useRouter()
+  const params = useSearchParams()
+  const type = params.get('type')
+
+  const typeFilter = useMemo(() => {
+    if (type) {
+      return type
+    }
+    return 'all'
+  }, [type])
+
+  const setFilter = (type: string) => {
+    if (type) {
+      router.push(`?type=${type}`)
+    }
+
+    if (!type) {
+      router.push('/coding')
+    }
+  }
+
   return (
     <div className="space-y-3 flex flex-col">
-      <div className="flex space-x-3">
-        <Toggle variant={'outline'} className="space-x-2">
-          <SquareChevronRight size={15} className="text-muted-foreground" />
-          <TypographyMuted className="text-sm">JavaScript</TypographyMuted>
-        </Toggle>
-        <Toggle variant={'outline'} className="space-x-2">
-          <PanelTop size={15} className="text-muted-foreground" />
-          <TypographyMuted className="text-sm">User Interface</TypographyMuted>
-        </Toggle>
-        <Toggle variant={'outline'} className="space-x-2">
-          <LayoutGrid size={15} className="text-muted-foreground" />
-          <TypographyMuted className="text-sm">Other</TypographyMuted>
-        </Toggle>
-      </div>
-      <div className="bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <form>
+      <div className="flex space-x-3 ">
+        <div className="grow bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
           <div className="relative">
             <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
             <Input placeholder="Search" className="pl-8" />
           </div>
-        </form>
+        </div>
+        <ToggleGroup value={typeFilter} type="single" variant="outline" defaultValue="">
+          <ToggleGroupItem onClick={() => setFilter('')} value="all" className="space-x-2">
+            <List size={15} className="text-muted-foreground" />
+            <TypographyMuted className="text-sm">All</TypographyMuted>
+          </ToggleGroupItem>
+          <ToggleGroupItem onClick={() => setFilter('javascript')} value="javascript" className="space-x-2">
+            <SquareChevronRight size={15} className="text-muted-foreground" />
+            <TypographyMuted className="text-sm">Coding</TypographyMuted>
+          </ToggleGroupItem>
+          <ToggleGroupItem onClick={() => setFilter('user_interface')} value="user_interface" className="space-x-2">
+            <PanelTop size={15} className="text-muted-foreground" />
+            <TypographyMuted className="text-sm">User Interface</TypographyMuted>
+          </ToggleGroupItem>
+        </ToggleGroup>
       </div>
     </div>
   )
