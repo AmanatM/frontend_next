3f6902715f78c5b59fda414853f79a0849de7964
Ama <24210834+AmanatM@users.noreply.github.com>
2024-05-30 18:40:44 -0700

    added progress counter

--- src/app/(dashboard)/(home)/page.tsx
+++ src/app/(dashboard)/(home)/page.tsx
@@ -6,6 +6,8 @@ import { Card } from '@/components/ui/card'
 import { createClientServer } from '@/supabase-utils/supabase-server'
 import { Metadata } from 'next'
 import CreateAccountCTA from './_components/CreateAccountCTA'
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
+import { getCodingQuestions } from '../coding/_queries/getCodingQuestions'
 
 export const metadata: Metadata = {
   title: 'Web Coders Lab - Learn web development for free',
@@ -14,6 +16,15 @@ export const metadata: Metadata = {
 export default async function Home() {
   const supabase = createClientServer()
   const user = (await supabase.auth.getUser()).data.user
+
+  //const user = (await supabase.auth.getUser()).data.user
+
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery({
+    queryKey: ['codingQuestions'],
+    queryFn: () => getCodingQuestions(supabase),
+  })
 
   return (
     <DashboardContainer className="space-y-8">
@@ -24,8 +35,7 @@ export default async function Home() {
         </div>
         {!user && <CreateAccountCTA />}
       </div>
-
-      {user && <ProgressSimple user={user} />}
+      <HydrationBoundary state={dehydrate(queryClient)}>{user && <ProgressSimple user={user} />}</HydrationBoundary>
     </DashboardContainer>
   )
 }
--- src/components/progress-simple.tsx
+++ src/components/progress-simple.tsx
@@ -1,31 +1,55 @@
+'use client'
 import { User } from '@supabase/auth-js'
 import { Card } from './ui/card'
 import { Progress } from './ui/progress'
-
-const mockData = [
-  {
-    title: 'User Interface',
-    completed: 22,
-    totalAmount: 89,
-  },
-  {
-    title: 'JavaScript',
-    completed: 80,
-    totalAmount: 120,
-  },
-  {
-    title: 'Tutorials',
-    completed: 0,
-    totalAmount: 32,
-  },
-]
+import { createClientServer } from '@/supabase-utils/supabase-server'
+import useSupabaseBrowser from '@/supabase-utils/supabase-client'
+import { useQuery } from '@tanstack/react-query'
+import { getCodingQuestions } from '@/app/(dashboard)/coding/_queries/getCodingQuestions'
+import { useMemo } from 'react'
 
 function ProgressSimple({ user }: { user: User | null }) {
+  const supabase = useSupabaseBrowser()
+
+  const { data: codingQuestions, isSuccess } = useQuery({
+    queryKey: ['codingQuestions'],
+    queryFn: () => getCodingQuestions(supabase),
+  })
+
+  const getQuestionCount = ({ type, userCompleted }: { type: string; userCompleted?: boolean }) => {
+    if (userCompleted) {
+      return (
+        codingQuestions?.filter(
+          question => question.question_type === type && question.user_completed_code_question.length > 0,
+        ).length || 0
+      )
+    }
+    return codingQuestions?.filter(question => question.question_type === type).length || 0
+  }
+
+  const templateData = [
+    {
+      title: 'User Interface',
+      completed: getQuestionCount({ type: 'user_interface', userCompleted: true }),
+      totalAmount: getQuestionCount({ type: 'user_interface' }),
+    },
+    {
+      title: 'JavaScript',
+      completed: getQuestionCount({ type: 'javascript', userCompleted: true }),
+      totalAmount: getQuestionCount({ type: 'javascript' }),
+    },
+    {
+      title: 'Tutorials',
+      completed: 0,
+      totalAmount: 2,
+    },
+  ]
+
   if (!user) return null
 
   return (
     <Card className="grid md:grid-cols-3 grid-cols-1 gap-y-6 gap-x-6 px-6 py-4">
-      {mockData.map((data, index) => (
+      {templateData.map((data, index) => (
         <div key={index} className="grid grid-row gap-y-2">
           <p className="text-sm font-medium mb-2">{data.title}</p>
           <div className="flex flex-column items-center gap-x-4">
