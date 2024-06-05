b66442da5cf22cb60e76fb55334e3ff8ceb09d4b
Ama <24210834+AmanatM@users.noreply.github.com>
2024-06-05 07:59:57 -0700

    tutorials: interactive components

--- src/app/tutorials/how-to-center-a-div/components/custom.tsx
+++ src/app/tutorials/how-to-center-a-div/components/custom.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+"use client"
+import React, { useState } from "react"
 
-function Custom() {
-  return <button>custom</button>;
+const Custom: React.FC = () => {
+  const [width, setWidth] = useState(50)
+
+  const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setWidth(Number(event.target.value))
+  }
+
+  return (
+    <div className="p-6">
+      <div className="flex flex-col lg:flex-row lg:space-x-6">
+        <div className="lg:w-1/2 mb-6 lg:mb-0">
+          <h2 className="text-2xl font-semibold mb-4">CSS Code</h2>
+          <pre className="bg-gray-100 p-4 rounded-md">
+            <code className="hljs language-css">{`
+.item {
+  width: ${width}%;
+  margin: 0 auto;
+  background-color: lightblue;
 }
 
-export default Custom;
+            `}</code>
+          </pre>
+        </div>
+        <div className="lg:w-1/2">
+          <h2 className="text-2xl font-semibold mb-4">Interactive CSS Width Slider</h2>
+          <input type="range" min="0" max="100" value={width} onChange={handleSliderChange} className="w-full mb-4" />
+          <div
+            className="bg-blue-300 p-10 mx-auto transition-all duration-300 ease-in-out"
+            style={{ width: `${width}%` }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Custom
