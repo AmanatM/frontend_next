68bdcf7bbfd2a1044eed2b56f2a8da360e0c9ca8
Ama <24210834+AmanatM@users.noreply.github.com>
2024-06-08 10:33:07 -0700

    tutorials: custom comonents in blog

--- src/app/tutorials/how-to-center-a-div/components/codeWithPreview.tsx
+++ src/app/tutorials/how-to-center-a-div/components/codeWithPreview.tsx
@@ -4,27 +4,38 @@ import { cn } from "@/lib/utils"
 import React, { useState } from "react"
 import { Slider } from "@/components/ui/slider"
 
-const CodeWithPreview = ({ code }: { code: string }) => {
-  const [width, setWidth] = useState([100])
-  const [height, setHeight] = useState([100])
+type CodeWithPreviewProps = {
+  code: string
+  containerStyle?: string
+  childStyle?: string
+  headerSingleColumn?: boolean
+  childContent?: React.ReactNode | string
+}
+
+export default function CodeWithPreview({
+  code,
+  containerStyle,
+  childStyle,
+  headerSingleColumn,
+  childContent = "Child",
+}: CodeWithPreviewProps) {
+  const [containerWidth, setContainerWidth] = useState([100])
+  const [containerHight, setContainerHeight] = useState([100])
+
+  const twoColsLayoutStyles = "grid grid-cols-1 md:grid-cols-2 md:gap-x-4 gap-y-8"
 
   return (
-    <div className="py-6 px-4 rounded-md">
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-4 gap-y-4 mb-12">
-        <div>
-          <div>
-            Container width: <span>{width + "%"}</span>
-          </div>
-          <Slider defaultValue={width} onValueChange={setWidth} min={30} />
-        </div>
-        <div>
-          <div>
-            Container height: <span>{height + "%"}</span>
-          </div>
-          <Slider defaultValue={height} onValueChange={setHeight} min={30} />
-        </div>
+    <div className="py-6 px-4 rounded-md bg-card">
+      {/* Header  */}
+      <div className={cn(twoColsLayoutStyles, "mb-12", headerSingleColumn && "md:grid-cols-1 grid-cols-1")}>
+        <SizeSlider value={containerWidth[0]} setValue={setContainerWidth} label="Container width" />
+        {!headerSingleColumn && (
+          <SizeSlider value={containerHight[0]} setValue={setContainerHeight} label="Container height" />
+        )}
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-4 gap-y-4 mb-8 md:mb-4">
+
+      {/* Code and preview  */}
+      <div className={cn(twoColsLayoutStyles, "mb-8 md:mb-4")}>
         <div className="size-full">
           <CodeHighlighter
             code={code}
@@ -35,15 +46,22 @@ const CodeWithPreview = ({ code }: { code: string }) => {
             }}
           />
         </div>
-        <div className="size-full bg-[#1e1e1e] rounded-md flex justify-center items-center relative  min-h-[150px]">
+        <div className="size-full bg-[#1e1e1e] rounded-md flex justify-center items-center relative min-h-[150px]">
           <div
-            className={cn("bg-slate-50 rounded-md text-neutral-900", "flex items-center justify-center")}
+            className={cn("bg-zinc-300 rounded-md text-neutral-900", containerStyle && containerStyle)}
             style={{
-              width: `${width}%`,
-              height: `${height}%`,
+              width: `${containerWidth}%`,
+              height: `${containerHight}%`,
             }}
           >
-            <div className="border border-stone-950 h-24 w-24 flex justify-center items-center">Child </div>
+            <div
+              className={cn(
+                "border-4 border-stone-950 h-24 w-24 flex justify-center items-center",
+                childStyle && childStyle,
+              )}
+            >
+              {childContent}
+            </div>
           </div>
         </div>
       </div>
@@ -51,4 +69,14 @@ const CodeWithPreview = ({ code }: { code: string }) => {
   )
 }
 
-export default CodeWithPreview
+function SizeSlider({ value, setValue, label }: { value: number; setValue: (value: number[]) => void; label: string }) {
+  return (
+    <div>
+      <div className="flex">
+        <span>Container width: </span>
+        <span className="ml-auto">{value + "%"}</span>
+      </div>
+      <Slider defaultValue={[value]} onValueChange={setValue} min={30} />
+    </div>
+  )
+}
